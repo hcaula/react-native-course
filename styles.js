cac6dee1180786bd044927bb2e9bc726efc2dca5
Henrique Caúla <lhtc@cin.ufpe.br>
2019-11-11 23:46:36 -0300

    Initial commit

---
+++ styles.js
@@ -0,0 +1,104 @@
+export default {
+  html: { margin: "0", height: "100%", width: "100%" },
+  body: {
+    fontFamily:
+      "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif"
+  },
+  app: { margin: "0", height: "100%", width: "100%" },
+  ul: { padding: "0" },
+  li: { listStyleType: "none" },
+  container: { maxWidth: "1200px", margin: "0 auto", padding: "50px" },
+  dark: { color: "#DADADA", background: "#1c2022", minHeight: "100%" },
+  flex_center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  btn_clear: { border: "none", background: "transparent" },
+  nav_link: {
+    fontSize: "18px",
+    fontWeight: "bold",
+    textDecoration: "none",
+    color: "inherit"
+  },
+  nav_li: { marginRight: "10px" },
+  grid: { display: "flex", flexWrap: "wrap" },
+  space_around: { justifyContent: "space-around" },
+  space_between: { justifyContent: "space-between" },
+  header_lg: { fontSize: "35px", fontWeight: "300", margin: "20px" },
+  header_sm: { fontSize: "28px", fontWeight: "300", margin: "10px" },
+  avatar: {
+    width: "150px",
+    height: "150px",
+    borderRadius: "3px",
+    margin: "0 auto",
+    display: "block"
+  },
+  center_text: { textAlign: "center" },
+  link: {
+    color: "rgb(187, 46, 31)",
+    textDecoration: "none",
+    fontWeight: "bold"
+  },
+  card_list: { margin: "20px 0", fontSize: "20px" },
+  card_list_li: { display: "flex", alignItems: "center", margin: "10px" },
+  card_list_svg: { marginRight: "10px" },
+  card_list_a: { fontWeight: "500", color: "inherit" },
+  bg_light: { background: "rgba(0, 0, 0, 0.08)", borderRadius: "3px" },
+  card: { margin: "10px 0", width: "250px", padding: "20px" },
+  card_a: { textDecoration: "none" },
+  card_img: { marginBottom: "8px" },
+  instructions_container: { margin: "100px 0" },
+  container_sm: { width: "80%", margin: "0 auto" },
+  battle_instructions: { padding: "0", fontSize: "25px" },
+  battle_instructions_li: { flex: "1", minWidth: "300px" },
+  battle_instructions_svg: { padding: "40px", borderRadius: "3px" },
+  column: { display: "flex", flexDirection: "column" },
+  row: { display: "flex", flexDirection: "row" },
+  player: { flex: "1", margin: "0 20px", padding: "10px" },
+  player_label: { fontSize: "20px", margin: "5px 0", fontWeight: "300" },
+  player_inputs_input: {
+    padding: "8px",
+    fontSize: "16px",
+    flex: "2",
+    borderRadius: "3px",
+    border: "none",
+    boxShadow: "inset 0 1px 2px rgba(0, 0, 0, 0.15)"
+  },
+  player_inputs__input_light: { background: "rgba(0, 0, 0, 0.02)" },
+  player_inputs_button: { flex: "1", marginLeft: "10px" },
+  btn: {
+    padding: "10px",
+    textDecoration: "none",
+    letterSpacing: ".25em",
+    borderRadius: "3px",
+    border: "none",
+    fontSize: "16px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    maxWidth: "200px"
+  },
+  dark_btn: { color: "#e6e6e6", background: "#141414" },
+  dark_btn_disabled: { background: "#f2f2f2", color: "#c7c7c7" },
+  players_container: { margin: "100px 0" },
+  avatar_small: { width: "55px", height: "55px", borderRadius: "50%" },
+  player_info: {
+    display: "flex",
+    flex: "1",
+    alignItems: "center",
+    fontSize: "20px",
+    padding: "10px"
+  },
+  player_info__link: { marginLeft: "10px" },
+  btn_space: { margin: "40px auto" },
+  error: { color: "#ff1616", fontSize: "20px", margin: "50px 0" },
+  bg_dark: { background: "rgb(36, 40, 42)", borderRadius: "3px" },
+  player_inputs__input_dark: {
+    color: "#DADADA",
+    background: "rgba(0, 0, 0, 0.3)"
+  },
+  light_btn: { color: "#000", background: "#aaa8a8" },
+  light_btn_disabled: { background: "#292929", color: "#4a4a4a" }
+};
